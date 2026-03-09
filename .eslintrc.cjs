f9884bd52d796e0cafe01c0ef48c87b00f727d80
Daniel Correa <yo@danielgara.com>
2026-03-09 15:41:11 -0500

    imp: linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2022: true },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "script",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  ignorePatterns: ["node_modules/", "sum.js"],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
+};
